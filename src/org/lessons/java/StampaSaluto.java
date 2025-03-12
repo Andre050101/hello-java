136dbbc5ad93975890ce7ff56e71ce25961f3727
Andre050101 <agiancristiano90@gmail.com>
2025-03-12 12:21:40 +0100

    package and StampaSaluto class created

---
+++ src/org/lessons/java/StampaSaluto.java
@@ -0,0 +1,7 @@
+package org.lessons.java;
+
+public class StampaSaluto {
+    public static void main (String[] args){
+        System.out.println("Buongiorno e benvenuto nel corso di specializzazione su Java Back-end con Spring");
+    };
+}
